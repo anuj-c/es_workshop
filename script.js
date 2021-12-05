75a3d8f2970e968614f9093c4badfd9738adef6b
anuj-c <57581295+anuj-c@users.noreply.github.com>
2021-12-05 10:56:47 +0530

    delete and edit

--- script.js
+++ script.js
@@ -21,5 +21,24 @@ const descButton = (e) => {
 }
 
 const editTodo = (e) => {
-  
+  e.nextElementSibling.classList.remove("d-none")
+  let parent = e.parentElement.previousElementSibling
+  parent.querySelector(".title").classList.add("d-none")
+  parent.querySelector(".editInput").classList.remove("d-none")
+}
+
+const saveTodo = (e) => {
+  let parent = e.parentElement.previousElementSibling
+  parent.querySelector(".title").innerText = parent.querySelector(".editInput").value
+  parent.querySelector(".title").classList.remove("d-none")
+  parent.querySelector(".editInput").classList.add("d-none")
+  e.classList.add("d-none")
+}
+
+const deleteTodo = (e) => {
+  e.parentElement.parentElement.remove()
+}
+
+const addTodo = () => {
+  let div1 = document.createElement("DIV")
 }
